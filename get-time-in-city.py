861eee2baff9801abdfb55b6db3c0e2c9a26ce0d
Dylan Lange <dylan@Michelles-MacBook-Air.local>
2023-12-10 23:27:28 +0100

    Multi word cities works again

--- get-time-in-city.py
+++ get-time-in-city.py
@@ -1,3 +1,4 @@
+import argparse
 from datetime import datetime
 import pytz
 
@@ -33,13 +34,18 @@ def get_time_in_city(city, user_time=None, debug=False):
         # Get the dynamically generated city time zones
         city_timezones = get_city_timezones()
 
-        # Make the input city name case-insensitive
-        city_lower = city.lower()
+        # Try to find the exact city name
+        city_timezone = city_timezones.get(city.lower())
 
-        # Define the timezone for the user's city using the mapping
-        city_timezone = city_timezones.get(city_lower)
-        if not city_timezone:
-            raise ValueError(f"Timezone not found for {city}")
+        # If not found, try to find a city that contains the input
+        if city_timezone is None:
+            matching_cities = [c for c in city_timezones.keys() if city.lower() in c]
+            if len(matching_cities) == 1:
+                city_timezone = city_timezones[matching_cities[0]]
+            elif len(matching_cities) > 1:
+                raise ValueError(f"Ambiguous city name. Matches: {matching_cities}")
+            else:
+                raise ValueError(f"Timezone not found for {city}")
 
         # Convert the user's time to the city's timezone
         city_time = user_time.astimezone(pytz.timezone(city_timezone))
@@ -52,22 +58,22 @@ def get_time_in_city(city, user_time=None, debug=False):
         return f"Error: {ve}"
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Get the current time in a specified city.")
+    parser.add_argument("city", nargs='+', help="The name of the city.")
+    parser.add_argument("--time", help="Specify the time in 'YYYY-MM-DD HH:MM:SS' format.")
+    parser.add_argument("--debug", action="store_true", help="Show available time zones for debugging.")
+
+    args = parser.parse_args()
+
     try:
-        # Get user input for time
-        user_input_time = input("Enter the time in the format 'YYYY-MM-DD HH:MM:SS' (Press Enter for current time): ")
-
-        # Set the default city to "Johannesburg"
-        city_name = input(f"Enter the city name (default is Johannesburg): ") or "Johannesburg"
-
-        # Ask the user if debugging information should be shown
-        debug_toggle = input("Show available time zones for debugging? (yes/no, default is no): ").lower()
-        debug = debug_toggle == 'yes'
-
         # Call the function to get the time in the specified city
-        result = get_time_in_city(city_name, user_input_time, debug=debug)
+        result = get_time_in_city(' '.join(args.city), args.time, debug=args.debug)
 
         # Print the result
         print(result)
+
+    except ValueError as ve:
+        print(f"Error: {ve}")
 
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
